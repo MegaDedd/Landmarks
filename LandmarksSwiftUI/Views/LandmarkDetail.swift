65df3567411e5fb1527b4561abee14926cac4454
Konstantin Antonov <kostya-ant@yandex.ru>
2024-02-20 17:40:47 +0400

    added View

---
+++ LandmarksSwiftUI/Views/LandmarkDetail.swift
@@ -0,0 +1,50 @@
+//
+//  LandmarkDetail.swift
+//  LandmarksSwiftUI
+//
+//  Created by Константин on 20.02.2024.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    
+    var landmark: Landmark
+    
+    var body: some View {
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate)
+                .frame(height: 300)
+            
+            CircleImage(image: landmark.image)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+                    .fontWeight(.light)
+                HStack {
+                    Text(landmark.park)
+                    Spacer()
+                    Text(landmark.state)
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                
+                Text("About \(landmark.name)")
+                    .font(.title2)
+                Text(landmark.description)
+            }
+            .padding()
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    LandmarkDetail(landmark: landmarks[0])
+}
