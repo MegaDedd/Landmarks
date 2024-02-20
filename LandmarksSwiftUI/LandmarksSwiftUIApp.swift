4260880a285482a0be5f029d0e2cd6ee1fa8c1d2
Konstantin Antonov <kostya-ant@yandex.ru>
2024-02-20 15:39:05 +0400

    Initial Commit

---
+++ LandmarksSwiftUI/LandmarksSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksSwiftUIApp.swift
+//  LandmarksSwiftUI
+//
+//  Created by Константин on 20.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
