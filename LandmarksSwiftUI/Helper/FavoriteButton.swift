9b15c1dfbc913a36441ad94ad26b88487a60ecfb
Konstantin Antonov <kostya-ant@yandex.ru>
2024-02-21 23:44:10 +0400

    create a favorite button and adopt

---
+++ LandmarksSwiftUI/Helper/FavoriteButton.swift
@@ -0,0 +1,27 @@
+//
+//  FavoriteButton.swift
+//  LandmarksSwiftUI
+//
+//  Created by Константин on 20.02.2024.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
